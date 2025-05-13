c31bbf6669cfc356793c80151271b4cfe39315d5
vat332 <oporowymakus21@gmail.com>
2025-05-13 15:02:23 +0200

    update grid item title and add react-lottie dependency
    - Changed the title of grid item with id 5 to "Clean Code to podstawa".
    - Added react-lottie package and its type definitions to package.json and package-lock.json.

--- data/index.ts
+++ data/index.ts
@@ -41,7 +41,7 @@ export const gridItems = [
   },
   {
     id: 5,
-    title: "Fan Formuły 1",
+    title: "Clean Code to podstawa",
     description: "",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
