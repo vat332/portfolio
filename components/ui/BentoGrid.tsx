733fb91a7d815dae1feaf8e826f02b5ae8877e59
vat332 <oporowymakus21@gmail.com>
2025-05-13 14:31:46 +0200

    Refactor gridItems: Update titles, descriptions, and layout classes for improved clarity and design

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -23,33 +23,39 @@ export const BentoGridItem = ({
   className,
   title,
   description,
-  header,
-  icon,
+  img,
+  imgClassName,
+  titleClassName,
+  spareImg,
   id,
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
-  header?: React.ReactNode;
-  icon?: React.ReactNode;
   id: number;
+  img?: string;
+  imgClassName?: string;
+  titleClassName?: string;
+  spareImg?: string;
 }) => {
   return (
     <div
       className={cn(
-        "group/bento shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-3xl border border-neutral-200 bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none relative ",
+        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className
       )}
+      style={{
+        background: "rgb(2,0,36)",
+        backgroundColor:
+          "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(59,59,68,1) 26%, rgba(93,108,111,1) 100% ",
+      }}
     >
-      {header}
+      <div className={`${id === 6} && 'flex justify-center h-full`}></div>
       <div className="transition duration-200 group-hover/bento:translate-x-2">
-        {icon}
-        <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
-          {title}
-        </div>
-        <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
-          {description}
-        </div>
+        {title}
+      </div>
+      <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
+        {description}
       </div>
     </div>
   );
