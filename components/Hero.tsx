5c9859f23f963b86fef66c378b2b7a394d2a81ef
vat332 <oporowymakus21@gmail.com>
2025-05-13 13:17:19 +0200

    Add MagicButton component and integrate with Hero; update styles and dependencies

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,6 @@
 import { cn } from "@/utils/cn";
+import { FaLocationArrow } from "react-icons/fa6";
+import MagicButton from "./ui/MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
@@ -16,13 +18,13 @@ const Hero = () => {
         />
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
-      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
+      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white bg-grid-black  flex items-center justify-center absolute top-0 left-0">
         <div
           className={cn(
             "absolute inset-0",
             "[background-size:100px_100px]",
-            "[background-image:linear-gradient(to_right,#e4e4e7_0.1px,transparent_0.8px),linear-gradient(to_bottom,#e4e4e7_0.1px,transparent_0.8px)]",
-            "white:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
+            "[background-image:linear-gradient(to_right,#e4e4e7_0.1px,transparent_0.55px),linear-gradient(to_bottom,#e4e4e7_0.1px,transparent_0.55px)]",
+            "white:[background-image:linear-gradient(to_right,#262626_0.1px,transparent_0.6px),linear-gradient(to_bottom,#262626_0.1px,transparent_0.6px)]"
           )}
         />
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
@@ -39,6 +41,13 @@ const Hero = () => {
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi, I&apos;m Sebastian
           </p>
+          <a href="#about" title="button-hero">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
         </div>
       </div>
     </div>
