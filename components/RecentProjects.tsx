d6ab6ce3540ee3e668713c0e5c7cec505813854a
vat332 <oporowymakus21@gmail.com>
2025-05-13 15:57:53 +0200

    Refactor code structure for improved readability and maintainability

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -19,7 +19,11 @@ const RecentProjects = () => {
                 <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                   <img src="/bg.png" alt="bg-img" />
                 </div>
-                <img src={img} alt={title} className="z-10 absolute bottom-0" />
+                <img
+                  src={img}
+                  alt={title}
+                  className="z-10 absolute bottom-0 rotate-5"
+                />
               </div>
               <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
                 {title}
