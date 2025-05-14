2d9ccfcf3d0c297f86254c262a9746be4bf30392
vat332 <oporowymakus21@gmail.com>
2025-05-14 13:12:17 +0200

    Update styles for dark mode support and adjust background properties in Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import { navItems } from "@/data";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
-      <div className="max-w-7xl w-full">
+      <div className="max-w-7xl w-full dark">
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,10 +24,10 @@ const Hero = () => {
             "absolute inset-0",
             "[background-size:100px_100px]",
             "dark:[background-image:linear-gradient(to_right,#e4e4e7_0.1px,transparent_0.55px),linear-gradient(to_bottom,#e4e4e7_0.1px,transparent_0.55px)]",
-            "[background-image:linear-gradient(to_right,#262626_0.1px,transparent_0.6px),linear-gradient(to_bottom,#262626_0.1px,transparent_0.6px)]"
+            "normal:[background-image:linear-gradient(to_right,#262626_0.1px,transparent_0.6px),linear-gradient(to_bottom,#262626_0.1px,transparent_0.6px)]"
           )}
         />
-        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
       </div>
       <div className="flex justify-center relative my-20 z-10 ">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
